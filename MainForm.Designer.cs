daf16a2661466b8bebbe83fbac06f3b85b441d95
Александр Шаповалов <ashsvis@gmail.com>
2025-09-10 11:48:45 +0300

    Заполнение деревьев первым уровнем

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,9 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             splitContainer1 = new SplitContainer();
+            tvSource = new TreeView();
+            statusStrip1 = new StatusStrip();
+            toolStrip1 = new ToolStrip();
+            tsbDefineRootSourcePath = new ToolStripButton();
+            tslRootSourcePath = new ToolStripLabel();
+            tvDestination = new TreeView();
+            statusStrip2 = new StatusStrip();
+            toolStrip2 = new ToolStrip();
+            tsbDefineRootDestinationPath = new ToolStripButton();
+            tslRootDestinationPath = new ToolStripLabel();
+            imageList1 = new ImageList(components);
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            toolStrip2.SuspendLayout();
             SuspendLayout();
             // 
             // splitContainer1
@@ -38,26 +55,154 @@
             splitContainer1.Dock = DockStyle.Fill;
             splitContainer1.Location = new Point(0, 0);
             splitContainer1.Name = "splitContainer1";
-            splitContainer1.Size = new Size(800, 450);
-            splitContainer1.SplitterDistance = 400;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(tvSource);
+            splitContainer1.Panel1.Controls.Add(statusStrip1);
+            splitContainer1.Panel1.Controls.Add(toolStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tvDestination);
+            splitContainer1.Panel2.Controls.Add(statusStrip2);
+            splitContainer1.Panel2.Controls.Add(toolStrip2);
+            splitContainer1.Size = new Size(994, 547);
+            splitContainer1.SplitterDistance = 497;
             splitContainer1.TabIndex = 0;
+            // 
+            // tvSource
+            // 
+            tvSource.Dock = DockStyle.Fill;
+            tvSource.ImageIndex = 0;
+            tvSource.ImageList = imageList1;
+            tvSource.Location = new Point(0, 25);
+            tvSource.Name = "tvSource";
+            tvSource.SelectedImageIndex = 0;
+            tvSource.Size = new Size(497, 500);
+            tvSource.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(0, 525);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(497, 22);
+            statusStrip1.SizingGrip = false;
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { tsbDefineRootSourcePath, tslRootSourcePath });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(497, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbDefineRootSourcePath
+            // 
+            tsbDefineRootSourcePath.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbDefineRootSourcePath.Image = (Image)resources.GetObject("tsbDefineRootSourcePath.Image");
+            tsbDefineRootSourcePath.ImageTransparentColor = Color.Magenta;
+            tsbDefineRootSourcePath.Name = "tsbDefineRootSourcePath";
+            tsbDefineRootSourcePath.Size = new Size(23, 22);
+            tsbDefineRootSourcePath.Text = "Определить корневую папку источника";
+            // 
+            // tslRootSourcePath
+            // 
+            tslRootSourcePath.Name = "tslRootSourcePath";
+            tslRootSourcePath.Size = new Size(17, 22);
+            tslRootSourcePath.Text = "\\\\";
+            // 
+            // tvDestination
+            // 
+            tvDestination.Dock = DockStyle.Fill;
+            tvDestination.ImageIndex = 0;
+            tvDestination.ImageList = imageList1;
+            tvDestination.Location = new Point(0, 25);
+            tvDestination.Name = "tvDestination";
+            tvDestination.SelectedImageIndex = 0;
+            tvDestination.Size = new Size(493, 500);
+            tvDestination.TabIndex = 2;
+            // 
+            // statusStrip2
+            // 
+            statusStrip2.Location = new Point(0, 525);
+            statusStrip2.Name = "statusStrip2";
+            statusStrip2.Size = new Size(493, 22);
+            statusStrip2.TabIndex = 1;
+            statusStrip2.Text = "statusStrip2";
+            // 
+            // toolStrip2
+            // 
+            toolStrip2.Items.AddRange(new ToolStripItem[] { tsbDefineRootDestinationPath, tslRootDestinationPath });
+            toolStrip2.Location = new Point(0, 0);
+            toolStrip2.Name = "toolStrip2";
+            toolStrip2.Size = new Size(493, 25);
+            toolStrip2.TabIndex = 0;
+            toolStrip2.Text = "toolStrip2";
+            // 
+            // tsbDefineRootDestinationPath
+            // 
+            tsbDefineRootDestinationPath.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            tsbDefineRootDestinationPath.Image = (Image)resources.GetObject("tsbDefineRootDestinationPath.Image");
+            tsbDefineRootDestinationPath.ImageTransparentColor = Color.Magenta;
+            tsbDefineRootDestinationPath.Name = "tsbDefineRootDestinationPath";
+            tsbDefineRootDestinationPath.Size = new Size(23, 22);
+            tsbDefineRootDestinationPath.Text = "Определить корневую папку назначения";
+            // 
+            // tslRootDestinationPath
+            // 
+            tslRootDestinationPath.Name = "tslRootDestinationPath";
+            tslRootDestinationPath.Size = new Size(17, 22);
+            tslRootDestinationPath.Text = "\\\\";
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "open.png");
+            imageList1.Images.SetKeyName(1, "newpage.png");
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(994, 547);
             Controls.Add(splitContainer1);
             Name = "MainForm";
-            StartPosition = FormStartPosition.WindowsDefaultBounds;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Репликация файлов и папок";
+            FormClosing += MainForm_FormClosing;
+            Load += MainForm_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            toolStrip2.ResumeLayout(false);
+            toolStrip2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private SplitContainer splitContainer1;
+        private TreeView tvSource;
+        private StatusStrip statusStrip1;
+        private ToolStrip toolStrip1;
+        private TreeView tvDestination;
+        private StatusStrip statusStrip2;
+        private ToolStrip toolStrip2;
+        private ToolStripButton tsbDefineRootSourcePath;
+        private ToolStripButton tsbDefineRootDestinationPath;
+        private ToolStripLabel tslRootSourcePath;
+        private ToolStripLabel tslRootDestinationPath;
+        private ImageList imageList1;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -2,9 +2,73 @@ namespace Replication
 {
     public partial class MainForm : Form
     {
+        private string rootSourcePath = string.Empty;
+        private string rootDestinationPath = string.Empty;
+
         public MainForm()
         {
             InitializeComponent();
+            var location = Properties.Settings.Default.MainFormLocation;
+            var size = Properties.Settings.Default.MainFormSize;
+            if (location.IsEmpty || size.IsEmpty)
+                StartPosition = FormStartPosition.CenterScreen;
+            else
+            {
+                StartPosition = FormStartPosition.Manual;
+                Location = location;
+                Size = size;
+            }
+            rootSourcePath = Properties.Settings.Default.RootSourcePath;
+            if (string.IsNullOrEmpty(rootSourcePath) )
+                rootSourcePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            tslRootSourcePath.Text = rootSourcePath;
+            rootDestinationPath = Properties.Settings.Default.RootDestinationPath;
+            tslRootDestinationPath.Text = rootDestinationPath;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadTree(tvSource, rootSourcePath);
+            LoadTree(tvDestination, rootDestinationPath);
+        }
+
+        private void LoadTree(TreeView tv, string rootPath)
+        {
+            tv.Nodes.Clear();
+            if (string.IsNullOrEmpty(rootPath)) return;
+            var nRoot = new TreeNode(rootPath) { Tag = rootPath };
+            tv.Nodes.Add(nRoot);
+            var folders = Directory.GetDirectories(rootPath);
+            foreach (var folder in folders) 
+            {
+                var nfolder = new TreeNode(Path.GetRelativePath(rootPath, folder))
+                {
+                    Tag = folder,
+                    ImageIndex = 0,
+                    SelectedImageIndex = 0
+                };
+                nfolder.Nodes.Add("stub");
+                nRoot.Nodes.Add(nfolder);
+            }
+            var files = Directory.GetFiles(rootPath);
+            foreach (var file in files)
+            {
+                var nfile = new TreeNode(Path.GetRelativePath(rootPath, file))
+                {
+                    Tag = file,
+                    ImageIndex = 1,
+                    SelectedImageIndex = 1
+                };
+                nRoot.Nodes.Add(nfile);
+            }
+            nRoot.Expand();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.MainFormLocation = Location;
+            Properties.Settings.Default.MainFormSize = Size;
+            Properties.Settings.Default.Save();
         }
     }
 }
